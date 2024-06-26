023e9f9ff2088b08ac2288fa3093e0a5bfc6feb7
dibkb <dibas9110@gmail.com>
2024-06-27 03:07:09 +0530

    comose up

---
+++ fastapi/api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/test")
+def read_root():
+    return {"Fast API running 🚀"}
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", port=8000, log_level="info")
